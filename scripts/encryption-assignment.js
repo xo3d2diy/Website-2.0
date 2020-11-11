90d55be620f83e153dc3bd6afcb91e8fc42d3da9
Teena Sharma <655911@pdsb.net>
2020-11-11 15:39:18 -0500

    Update encryption-assignment.js

--- scripts/encryption-assignment.js
+++ scripts/encryption-assignment.js
@@ -11,7 +11,7 @@
         for( let i = 0 ; i < plaintext.length ; i = i + 1 ){ // for each word in the plaintext, do the following
           if (plaintext[i].length > 2) { // If the current word is 2 or more letters, do the following:
 
-            ciphertext += (plaintext[i]).replace(/a/g, "1").replace(/e/g, "5").replace(/i/g, "9").replace(/o/g, "15").replace(/u/g, "21"); // replace each vowel with the number placement in the alphabet eg.  e is the fifth letter of the alphabet
+            ciphertext += (plaintext[i]).replace(/a/g, "1").replace(/e/g, "5").replace(/i/g, "9").replace(/o/g, "16").replace(/u/g, "21"); // replace each vowel with the number placement in the alphabet eg.  e is the fifth letter of the alphabet
           
             ciphertext += firstletter + "bi "; // Add the first letter and "bi" to the end of the current word 
           } else {
@@ -30,7 +30,7 @@
         let cipherList = ciphertext.split(" ");
         for( let i = 0 ; i < cipherList.length ; i = i + 1 ){ // for each word in the ciphertext, do the following
             if (cipherList[i].length > 2) { // If the current word is 2  or more letters, do the following:
-                plaintext += (cipherList[i]).replace(/1/g, "a").replace(/5/g, "e").replace(/9/g, "i").replace(/"15"/g, "o").replace(/u/g, "21").replace(/bi/g, " "); // replace these numbers with the the special value 
+                plaintext += (cipherList[i]).replace(/1/g, "a").replace(/5/g, "e").replace(/9/g, "i").replace(/"16"/g, "o").replace(/u/g, "21").replace(/bi/g, " "); // replace these numbers with the the special value 
               console.log ("testing if statement")
                // plaintext += firstletter; // Add the first letter and "bi" to the end of the current word 
               } else {
