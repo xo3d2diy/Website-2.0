1baf236178072e3635c4041d91ff580f00e8679c
Teena Sharma <655911@pdsb.net>
2020-11-12 12:41:55 -0500

    buttons

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,3 +1,8 @@
+
+
+
+//BUTTONS ASSIGNMENT 
+
 function sayHello() {
     alert("Hi there welcome to my site!");
 }
@@ -5,15 +10,53 @@ function sayGoodbye() {
 alert (" Bye, come back later! ");
 }
 
-function greeting() {
-   let name= prompt("Whats your name?")
-   alert ("Hello" +  name);
+
+//mini questionnaire buttons
+function question1(){
+    let name = prompt("HELLO NICE TO MEET YOU! What's your name?");
+    alert(name + "? What a lovely name! Nice to meet you, " + name + "!");
 }
 
-function vardemo() {
- let firstnumber = 9;
- let secondnumber = 85
- secondnumber = 6;
- let sum = firstnumber - secondnumber 
- console.log(answer)
+function question2() {
+    let colour = prompt(" What's your favourite colour?");
+    alert("Ah yes, I love the colour " + colour ); 
 }
+
+function question3() {
+    let animal = prompt("Last question I promise , what's your favourite animal?");
+    alert(animal + " are great :)");
+}
+
+
+
+//SOME MATH 
+function varDemo() {
+    alert("check the console and do some math..addition!")
+    let firstnumber = 15;
+    let secondnumber = 8;
+    let answer = firstnumber + secondnumber;
+    console.log(answer + " ...fun stuff here!");
+}
+
+function varDemo2() {
+    alert("check the console and do some math..but division this time!")
+    let firstnumber = 36
+    let secondnumber = 12
+    let answer = firstnumber / secondnumber
+    console.log(answer + " ...numbers are fun :)")
+}
+
+function varDemo3() {
+    alert("check the console and do some math..we're ending with some subtraction!")
+    let firstnumber = 100
+    let secondnumber = 37
+    let answer = firstnumber * secondnumber
+    console.log(answer + " ...woo math!")
+}
+
+//random button
+function midway() {
+    alert("Almost there!")
+}
+
+  
